e4cfd9dbc0ccf50a66799e3b7f50465bce9d5f37
chu-young <ttheko101@gmail.com>
2021-01-20 16:44:32 +0200

    initial project is complete

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ const mainContent = document.querySelector('.main-content');
 menuToggle.addEventListener('click', () =>{
     menuToggle.classList.toggle('active');
     mainContent.classList.toggle('active');
+    
 })
